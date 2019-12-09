0ad0795c8f341ad4ea3716f41392751dcd59ff59
donutdon18 <56841262+donutdon18@users.noreply.github.com>
2019-12-09 06:54:16 -0600

    first major additions to repository
    havent validated.
    problem with white space after adding booot
    problem with External JS carousel
    Don't know how to start the BLOG and Calendar portion

---
+++ swap.js
@@ -0,0 +1,14 @@
+var imagesAry=["mainTrain.jpg","image5.jpg","image6.jpg"];
+function showImg()
+var randomNumber=Math.floor(Math.random()*3);
+if(randomNumber==1){
+    document.getElementById("3pic").innerHTML=imagesAry[randomNumber]
+}
+else if(randomNumber==2){
+    document.getElementById("3pic").innerHTML=imagesAry[randomNumber]
+}
+else{
+    document.getElementById("3pic").innerHTML=imagesAry[randomNumber]
+}
+
+
