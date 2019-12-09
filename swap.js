5e65aad3c577392ff4ac6dc35cfcbb36db53940c
donutdon18 <sdonghwan@yahoo.com>
2019-12-09 13:22:36 -0600

    Fixed some
    White space is manageable
    gotta finish up External JS
    blog is for aside
    calendar is table

--- swap.js
+++ swap.js
@@ -1,14 +1,13 @@
-var imagesAry=["mainTrain.jpg","image5.jpg","image6.jpg"];
-function showImg()
-var randomNumber=Math.floor(Math.random()*3);
-if(randomNumber==1){
-    document.getElementById("3pic").innerHTML=imagesAry[randomNumber]
-}
-else if(randomNumber==2){
-    document.getElementById("3pic").innerHTML=imagesAry[randomNumber]
-}
-else{
-    document.getElementById("3pic").innerHTML=imagesAry[randomNumber]
-}
+var randomNumber = Math.floor(Math.random() * 2);
 
-
+function showImg() {  
+if (randomNumber == 0) {
+    document.getElementById("3pic").src = "mainTrain.jpg"
+} else if (randomNumber == 1) {
+    document.getElementById("3pic").src = "image5.jpg"
+} else {
+    document.getElementById("3pic").src = "image6.jpg"
+}
+function startTimer() {
+    setInterval(showImg,3);
+}}
